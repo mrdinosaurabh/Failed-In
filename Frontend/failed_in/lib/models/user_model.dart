6669cb1d3d2c25132f71869bf58a7c4efc125ed0
Harsh Gyanchandani <harsh.gyanchandani@gmail.com>
2021-10-09 10:59:53 +0530

    completed signup

---
+++ Frontend/failed_in/lib/models/user_model.dart
@@ -0,0 +1,21 @@
+class User {
+  late String? id;
+  late String? username;
+  late String? firstName;
+  late String? lastName;
+  late String? email;
+  late String? password;
+  late String? image;
+  late String? bio;
+
+  User({
+    this.id,
+    this.username,
+    this.firstName,
+    this.lastName,
+    this.email,
+    this.password,
+    this.image,
+    this.bio,
+  });
+}
---
+++ Frontend/failed_in/lib/utilities/server_details.dart
@@ -0,0 +1,4 @@
+const String hostname = '192.168.1.9';
+const int port = 8000;
+
+const String serverUrl = 'http://$hostname:$port';
