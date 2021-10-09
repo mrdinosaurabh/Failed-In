ab21f81354a4fe0ed1eaf5779d93e58ec2db155f
gursimran18 <kaurgursimran9918@gmail.com>
2021-10-09 06:02:09 +0530

    Added error-controller and set api

--- Backend/app.js
+++ Backend/app.js
@@ -3,13 +3,20 @@ const morgan = require('morgan');
 
 const app = express();
 
+const errorHandler = require('./controllers/errorController');
+const userRoutes = require('./routes/userRoutes');
+
 app.use(morgan('dev'));
 
 // Middleware to read the body of http post request
 app.use(express.json());
 
+//specifying routes
+app.use('/users', userRoutes);
+
 app.use('*', (req, res, next) => {
     res.status(404).send('This route is not defined!');
 });
+app.use(errorHandler);
 
 module.exports = app;
---
+++ Backend/controllers/errorController.js
@@ -0,0 +1,27 @@
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+        error: err,
+        stack: err.stack
+    });
+};
+
+const sendErrorProd = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
+};
+
+module.exports = (err, req, res, next) => {
+
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+    } else {
+        sendErrorProd(err, res);
+    }
+};
